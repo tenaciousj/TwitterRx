8188a6274ffa7744fe66a1b3971ff9bea4974520
jeanettepranin <jeanettepranin@gmail.com>
2015-11-07 20:04:47 -0500

    change port 4

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
         APP_TOKEN = get_app_token()
         APP_TOKEN['access_token']
     except: raise
-
-    app.run(port=8002, debug=True)
+    app.run()
+    # app.run(port=8002, debug=True)
     # port = int(os.environ.get("PORT", 8002))
     # app.run(host='0.0.0.0', port=port)
